2c5449e88cb0cb65bb23624b917231f2a9686bf2
Stuart133 <stuart1334@gmail.com>
2023-07-05 09:45:41 +0100

    Color the sphere according to its surface normals

--- src/ray.rs
+++ src/ray.rs
@@ -12,19 +12,29 @@ impl Ray {
         Self { origin, direction }
     }
 
+    #[inline]
     pub fn at(&self, t: f64) -> Point {
         self.origin + t * self.direction
+    }
+
+    #[inline]
+    pub fn direction(&self) -> &Vector {
+        &self.direction
     }
 
     pub fn color(&self) -> Color {
         let sphere = Sphere::new(Point::new(0.0, 0.0, -1.0), 0.5);
 
-        if sphere.hit(self) {
-            Color::new(1.0, 0.0, 0.0)
-        } else {
-            // The ray hit nothing, render the background gradient
-            let t = 0.5 * (Unit::new_normalize(self.direction).y + 1.0);
-            (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+        match sphere.hit(self) {
+            Some(t) => {
+                let normal = Unit::new_normalize(self.at(t) - sphere.center);
+                0.5 * Color::new(normal.x + 1.0, normal.y + 1.0, normal.z + 1.0)
+            }
+            None => {
+                // The ray hit nothing, render the background gradient
+                let t = 0.5 * (Unit::new_normalize(self.direction).y + 1.0);
+                (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+            }
         }
     }
 }
@@ -39,13 +49,18 @@ impl Sphere {
         Self { center, radius }
     }
 
-    pub fn hit(&self, ray: &Ray) -> bool {
+    pub fn hit(&self, ray: &Ray) -> Option<f64> {
         let oc = ray.origin - self.center;
-        let a = ray.direction.dot(&ray.direction);
-        let b = 2.0 * oc.dot(&ray.direction);
-        let c = oc.dot(&oc) - self.radius.powi(2);
+        let a = ray.direction().magnitude_squared();
+        let half_b = oc.dot(ray.direction());
+        let c = oc.magnitude_squared() - self.radius.powi(2);
 
         // Check the determinant of the intersection quadratic implies real solutions
-        return b.powi(2) - 4.0 * a * c > 0.0;
+        let discriminant = half_b.powi(2) - a * c;
+        if discriminant > 0.0 {
+            return Some((-half_b - discriminant.sqrt()) / a);
+        } else {
+            None
+        }
     }
 }
