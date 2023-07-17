26fc28bcd62edeb5e5f3aa4315c3e0c45c52a204
Stuart133 <stuart1334@gmail.com>
2023-07-17 09:44:04 +0100

    Smooth the noise

--- src/perlin.rs
+++ src/perlin.rs
@@ -24,11 +24,28 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: Point) -> f64 {
-        let i = (4.0 * p.x) as i32 & 255;
-        let j = (4.0 * p.y) as i32 & 255;
-        let k = (4.0 * p.z) as i32 & 255;
+        // TODO: Figure how this all works
+        let u = p.x - p.x.floor();
+        let v = p.y - p.y.floor();
+        let w = p.z - p.z.floor();
 
-        self.ran[self.perm_x[i as usize] ^ self.perm_y[j as usize] ^ self.perm_z[k as usize]]
+        let i = p.x.floor() as i32;
+        let j = p.y.floor() as i32;
+        let k = p.z.floor() as i32;
+        let mut c = [[[0.0; 2]; 2]; 2];
+
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di as usize][dj as usize][dk as usize] = self.ran[self.perm_x
+                        [((i + di) & 255) as usize]
+                        ^ self.perm_y[((j + dj) & 255) as usize]
+                        ^ self.perm_z[((k + dk) & 255) as usize]]
+                }
+            }
+        }
+
+        Self::trilinear_interpolate(&c, u, v, w)
     }
 
     fn generate_perm() -> Vec<usize> {
@@ -46,5 +63,18 @@ impl Perlin {
             p[i] = p[target];
             p[target] = tmp;
         }
+    }
+
+    fn trilinear_interpolate(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        (0..2).fold(0.0, |acc, i| {
+            (0..2).fold(acc, |acc, j| {
+                (0..2).fold(acc, |acc, k| {
+                    acc + (i as f64 * u + (1 - i) as f64 * (1.0 - u))
+                        * (j as f64 * v + (1 - j) as f64 * (1.0 - v))
+                        * (k as f64 * w + (1 - k) as f64 * (1.0 - w))
+                        * c[i][j][k]
+                })
+            })
+        })
     }
 }
